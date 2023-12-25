6b9870754f988ddb43c114453495872664cd1373
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2023-12-25 22:11:45 +0500

    create IDetectionService abstract class

---
+++ detection/services/IDetectionService.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from PIL.Image import Image
+
+from detection.protocols.part import DetectionPartProtocol
+
+
+class IDetectionService(ABC):
+    """Person detection service interface"""
+
+    @abstractmethod
+    def detect(
+        self,
+        image: Image,
+        min_percentage_probability: int = 50,
+    ) -> list[DetectionPartProtocol]:
+        pass
