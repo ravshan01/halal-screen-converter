45688414e8f04875743dd3fbaae77968bb949c9c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-01 20:50:47 +0500

    realized TestImageService.test_blur_boxes

--- images/services/images_test.py
+++ images/services/images_test.py
@@ -8,9 +8,28 @@ from mock.images import room
 class TestImageService:
     def test_blur(self):
         image = Image.open(room.resource.path)
-        image_np = numpy.array(image)
-
         blured_image = images_service.blur(image, percentage=50)
-        blured_image_np = numpy.array(blured_image)
 
-        assert not numpy.array_equal(image_np, blured_image_np)
+        assert not numpy.array_equal(numpy.array(image), numpy.array(blured_image))
+
+    def test_blur_boxes(self):
+        image = Image.open(room.resource.path)
+        boxes = [
+            (0, 0, 200, 200),
+            (400, 0, 600, 200),
+            (200, 200, 400, 400),
+            (500, 400, 900, 700),
+        ]
+        blured_image = images_service.blur_boxes(
+            image,
+            boxes,
+            percentage=50,
+        )
+
+        for box in boxes:
+            image_part = image.crop(box)
+            blured_part = blured_image.crop(box)
+
+            assert not numpy.array_equal(
+                numpy.array(image_part), numpy.array(blured_part)
+            )
