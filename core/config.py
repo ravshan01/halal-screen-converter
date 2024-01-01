32a6495eda0fb3470ea915be287e943ff943a3d7
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-01 11:12:43 +0500

    delete old weights logic

--- core/config.py
+++ core/config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    weights_path: str
     bot_token: str
 
 
