69e8172946130a154e73b4c07d36d4a7c16e5a50
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-01 16:33:21 +0500

    create TestImageService

--- images/services/images_test.py
+++ images/services/images_test.py
@@ -1,2 +1,16 @@
+import numpy
+from PIL import Image
+
+from .images import images_service
+from mock.images import room
+
+
 class TestImageService:
-    pass
+    def test_blur(self):
+        image = Image.open(room.resource.path)
+        image_np = numpy.array(image)
+
+        blured_image = images_service.blur(image, percentage=50)
+        blured_image_np = numpy.array(blured_image)
+
+        assert not numpy.array_equal(image_np, blured_image_np)
