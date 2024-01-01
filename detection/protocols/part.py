be150b6b6ce7e960e166ba92bab19070d27e4548
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-01 11:25:46 +0500

    DetectionPartProtocol.name: DetectionType

--- detection/protocols/part.py
+++ detection/protocols/part.py
@@ -1,7 +1,8 @@
 from typing import Protocol
+from detection.enums.type import DetectionType
 
 
 class DetectionPartProtocol(Protocol):
-    name: str
+    name: DetectionType
     score: float
     box_points: list[int]  # x1, y1, x2 and y2 coordinates
