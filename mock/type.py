71443fdc08580d5da7d66ebb0cb69cf3fd59882c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-01 12:02:52 +0500

    create MockResource dataclass

---
+++ mock/type.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from detection.enums.type import DetectionType
+
+
+class MockResourceEnum(Enum):
+    Image = "image"
+    Video = "video"
+
+
+@dataclass
+class MockResource:
+    type: MockResourceEnum
+    path: str
+    detections: dict[DetectionType, int]
