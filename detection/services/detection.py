37d5c3c370cc18d7aee99ef186900dc928763051
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-01 22:17:45 +0500

    update model and SCORE_THRESH_TEST in DetectionService

--- detection/services/detection.py
+++ detection/services/detection.py
@@ -16,20 +16,15 @@ setup_logger()
 class DetectionService(IDetectionService):
     predictor: DefaultPredictor
     person_type = 0
+    config_file = "COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml"
 
     def __init__(self):
         cfg = get_cfg()
         # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-        cfg.merge_from_file(
-            model_zoo.get_config_file(
-                "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-            )
-        )
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+        cfg.merge_from_file(model_zoo.get_config_file(self.config_file))
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.55  # set threshold for this model
         # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-            "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-        )
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(self.config_file)
         # TODO: check support cuda and use it
         cfg.MODEL.DEVICE = "cpu"
         self.predictor = DefaultPredictor(cfg)
