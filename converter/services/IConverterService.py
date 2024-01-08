7586cce65e03dfc9375b615d6f199809b8739c6b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-08 21:57:56 +0500

    create IConverterService.convert_media method

--- converter/services/IConverterService.py
+++ converter/services/IConverterService.py
@@ -2,8 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class IConverterService(ABC):
-    """Detect and blur persons on image"""
+    """Detect and blur persons in images and videos."""
 
     @abstractmethod
     def convert_image(self, image: bytes) -> bytes:
+        pass
+
+    @abstractmethod
+    def convert_video(self, video: bytes) -> bytes:
         pass
