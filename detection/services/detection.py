61ea76ca41dcd38fbcaec43f999532822ce024e9
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-14 10:25:29 +0500

    use ultralytics in DetectionService

--- detection/services/detection.py
+++ detection/services/detection.py
@@ -1,48 +1,33 @@
-import numpy
+from ultralytics import YOLO
 
-from detection.enums.type import DetectionType
 from detection.dataclasses.part import DetectionPart
+from detection.enums.type import DetectionType
 from detection.services.IDetectionService import IDetectionService
 from PIL import Image
 
-from detectron2.utils.logger import setup_logger
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-
-setup_logger()
-
 
 class DetectionService(IDetectionService):
-    predictor: DefaultPredictor
     person_type = 0
-    config_file = "COCO-Detection/faster_rcnn_R_50_DC5_3x.yaml"
 
     def __init__(self):
-        cfg = get_cfg()
-        # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-        cfg.merge_from_file(model_zoo.get_config_file(self.config_file))
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.55  # set threshold for this model
-        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(self.config_file)
-        # TODO: check support cuda and use it
-        cfg.MODEL.DEVICE = "cpu"
-        self.predictor = DefaultPredictor(cfg)
+        self.model = YOLO("yolov8s.pt")
 
     def detect(self, image: Image) -> list[DetectionPart]:
-        outputs = self.predictor(numpy.array(image))
-        person_instances = outputs["instances"][
-            outputs["instances"].pred_classes == self.person_type
-        ]
+        detects = self.model.predict(image, save=True)[0]
+
+        persons_indexes = []
+        for i, val in enumerate(detects.boxes.cls):
+            if val == self.person_type:
+                persons_indexes.append(i)
 
         return list(
             map(
                 lambda i: DetectionPart(
                     name=DetectionType.Person,
-                    score=person_instances.scores[i].item(),
-                    coords=tuple(*person_instances.pred_boxes[i].tensor.tolist()),
+                    score=detects.boxes.conf[i].item(),
+                    coords=tuple(detects.boxes.xyxy[i].tolist()),
                 ),
-                range(len(person_instances)),
+                persons_indexes,
             )
         )
 
