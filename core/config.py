8677fbc53a595b4f010cde3d5f509ecafbaf0702
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-15 12:06:03 +0500

    add 'weights_path' on core/config Settings

--- core/config.py
+++ core/config.py
@@ -1,9 +1,14 @@
+from pathlib import PurePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from settings import ROOT_DIR
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=str(PurePath(ROOT_DIR, ".env")), env_file_encoding="utf-8"
+    )
     bot_token: str
+    weights_path: str
 
 
 config = Settings()
