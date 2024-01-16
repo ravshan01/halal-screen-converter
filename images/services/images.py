0005b7863e0562860b481fc6986cc00bc12243b9
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-01-16 21:46:12 +0500

    update ImagesService.blur logic

--- images/services/images.py
+++ images/services/images.py
@@ -5,7 +5,15 @@ from images.services.IImagesService import IImagesService
 
 class ImagesService(IImagesService):
     def blur(self, image: Image, percentage: int = 50) -> Image:
-        return image.filter(ImageFilter.GaussianBlur(percentage))
+        # I haven't come up with anything else to calculate the radius
+        return image.filter(
+            ImageFilter.GaussianBlur(
+                (
+                    int((image.width * (percentage / 700))),
+                    int((image.height * (percentage / 700))),
+                )
+            )
+        )
 
     def blur_boxes(
         self, image: Image, boxes: list[tuple[int, int, int, int]], percentage: int = 50
