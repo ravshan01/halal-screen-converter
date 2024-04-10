5776045200da4029138e1dc4b45da4797e098608
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-04-10 15:43:24 +0500

    add 'port' in Settings

--- core/config.py
+++ core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=str(PurePath(ROOT_DIR, ".env")), env_file_encoding="utf-8"
     )
+
+    port: int
     bot_token: str
     weights_path: str
 
