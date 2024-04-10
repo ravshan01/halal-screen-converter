503a67ae722c2e3058f04fd45088ca482a1ee286
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-04-10 15:37:03 +0500

    start grpc server in main.py

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+from concurrent import futures
+from grpc import server as grpc_server
+
+from converter.services.converter import converter_service
+from halal_screen_proto.converter_service_pb2_grpc import (
+    add_ConverterServiceServicer_to_server,
+)
+
+from converter.servicer import ConverterServiceServicer
+
+
+def serve():
+    server = grpc_server(futures.ThreadPoolExecutor(max_workers=10))
+    add_ConverterServiceServicer_to_server(
+        ConverterServiceServicer(converter_service), server
+    )
+
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    print("Server started")
+
+    server.wait_for_termination()
+
+
 def main():
-    pass
+    print("Starting server")
+    serve()
+    print("Server stopped")
 
 
 if __name__ == "__main__":
